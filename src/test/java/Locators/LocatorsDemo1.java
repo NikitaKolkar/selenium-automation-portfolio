05e7a27d77749c940ceddf572f3b350b3a6c8ca0
NikitaKolkar <nikitakolkar27@gmail.com>
2025-07-21 13:48:02 +0530

    Initial commit

---
+++ src/test/java/Locators/LocatorsDemo1.java
@@ -0,0 +1,19 @@
+package Locators;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class LocatorsDemo1 {
+
+	public static void main(String[] args) {
+		//initiate the webdriver
+		WebDriver driver = new ChromeDriver();
+		//maximize the browser window
+		driver.manage().window().maximize();
+		
+	
+
+	}
+
+}
