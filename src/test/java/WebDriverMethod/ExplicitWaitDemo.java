ee8eedfded20525bc4ad339d1d074c9c21ee29e5
NikitaKolkar <nikitakolkar27@gmail.com>
2025-07-31 19:10:38 +0530

    Rename the classname

--- src/test/java/WebDriverMethod/ExplicitWaitDemo.java
+++ src/test/java/WebDriverMethod/ExplicitWaitDemo.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class ExpliciteWaitDemo {
+public class ExplicitWaitDemo {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
--- src/test/java/WebDriverMethod/ImplicitWaitMethods.java
+++ src/test/java/WebDriverMethod/ImplicitWaitMethods.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ImpliciteWaitMethods {
+public class ImplicitWaitMethods {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
