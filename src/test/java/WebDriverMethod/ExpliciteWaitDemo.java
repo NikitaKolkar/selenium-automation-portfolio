a5d066324e35fa24de2418d68a882b1342a080a5
NikitaKolkar <nikitakolkar27@gmail.com>
2025-07-31 18:56:06 +0530

    Added class for implicit and explicit wait

---
+++ src/test/java/WebDriverMethod/ExpliciteWaitDemo.java
@@ -0,0 +1,36 @@
+package WebDriverMethod;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExpliciteWaitDemo {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		
+		//explicit wait declaration
+		WebDriverWait mywait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		
+		driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+	    //Enter Username and password
+        WebElement userName = mywait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Username']")));
+        userName.sendKeys("Admin");
+        
+        WebElement password = mywait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Password']")));
+        password.sendKeys("admin123");
+		
+        WebElement loginButton = mywait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Login']")));
+		loginButton.click();
+		
+		driver.close();
+
+	}
+
+}
